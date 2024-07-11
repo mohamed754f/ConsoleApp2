62e0bf0e555eac4be329369b6da31603325e1a3f
Mohamed Galal <Mohamed Galal@DESKTOP-28D5B9Q>
2024-07-11 13:20:46 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your ID: ");
+            string Id = Console.ReadLine();
+
+            
+            Console.Write("Enter your name: ");
+            string Name = Console.ReadLine();
+
+            bool Number = false;
+            int age;
+            do
+            {
+                Console.Write("Enter your Age: ");
+                string Age = Console.ReadLine();
+                Number = int.TryParse(Age, out age);
+            } while (Number == false);
+
+            Console.WriteLine(Id);
+            Console.WriteLine(Name);
+            Console.WriteLine(age);
+        }
+    }
+}
